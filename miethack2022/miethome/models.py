a14e906763e130ac3cf8df4103ce4453ac7a8c52
ssWieldss <es130802@gmail.com>
2022-05-28 18:10:13 +0300

    Change numbers fields database type

--- miethack2022/miethome/models.py
+++ miethack2022/miethome/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class Room(models.Model):
     """Represents direction disciplines (subjects) in database"""
-    room_number = models.CharField(max_length=3)
-    corpus_number = models.CharField(max_length=3)
+    room_number = models.IntegerField(max_length=3)
+    corpus_number = models.IntegerField(max_length=3)
 
     def __str__(self):
         return f'{self.room_number}->{self.corpus_number}'
@@ -14,12 +14,12 @@ class Room(models.Model):
 
 class Student(models.Model):
     """Represents Man in database"""
-    id = models.BigIntegerField(max_length=10)
-    fio = models.CharField(max_length=100)
+    id = models.IntegerField(max_length=10)
+    full_name = models.CharField(max_length=100)
     birth_date = models.CharField(max_length=10)
     img = models.ImageField(upload_to='static/img/students_photo/')
-    home_order_number = models.BigIntegerField(max_length=20)
-    enrollment_order_number = models.BigIntegerField(max_length=10)
+    home_order_number = models.CharField(max_length=20)
+    enrollment_order_number = models.CharField(max_length=10)
     date_enrollment = models.CharField(max_length=10)
     birth_place = models.CharField(max_length=100)
     room = models.ForeignKey(Room, on_delete=models.SET_NULL)
