e9acb637c3c652779bd29f7b6f10cc0b89d3810a
Georgij Krajnyukov <nrk63@yandex.ru>
2022-05-29 15:27:45 +0300

    Made interactive scheme

--- miethack2022/miethome/urls.py
+++ miethack2022/miethome/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', index, name='home'),
     path('schematic_search', schematic_search, name='schematic_search'),
     path('add_student', add_student, name='add_student'),
+    path('get_floor', get_floor, name="get_floor"),
+    path('get_rooms', get_rooms, name="get_rooms"),
 ]
